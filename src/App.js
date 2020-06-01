cce033c898e49d79fb5417ed5a796782fd9bb234
Nitesh Sharma <nitesh.grep@gmail.com>
2020-06-01 09:11:03 +0530

    Creating component and passing data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,141 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import BookSegment from './components/BookSegment'
+
 
 class BooksApp extends React.Component {
   state = {
+    "categories":["Currently Reading"],
+    "Currently Reading":[{
+      "title": "Travels",
+      "authors": [
+          "Michael Crichton"
+      ],
+      "publisher": "Vintage",
+      "publishedDate": "2012-05-14",
+      "description": "From the bestselling author of Jurassic Park, Timeline, and Sphere comes a deeply personal memoir full of fascinating adventures as he travels everywhere from the Mayan pyramids to Kilimanjaro. Fueled by a powerful curiosity—and by a need to see, feel, and hear, firsthand and close-up—Michael Crichton's journeys have carried him into worlds diverse and compelling—swimming with mud sharks in Tahiti, tracking wild animals through the jungle of Rwanda. This is a record of those travels—an exhilarating quest across the familiar and exotic frontiers of the outer world, a determined odyssey into the unfathomable, spiritual depths of the inner world. It is an adventure of risk and rejuvenation, terror and wonder, as exciting as Michael Crichton's many masterful and widely heralded works of fiction.",
+      "industryIdentifiers": [
+          {
+              "type": "ISBN_13",
+              "identifier": "9780307816498"
+          },
+          {
+              "type": "ISBN_10",
+              "identifier": "0307816494"
+          }
+      ],
+      "readingModes": {
+          "text": true,
+          "image": false
+      },
+      "pageCount": 400,
+      "printType": "BOOK",
+      "categories": [
+          "Biography & Autobiography"
+      ],
+      "averageRating": 3.5,
+      "ratingsCount": 12,
+      "maturityRating": "NOT_MATURE",
+      "allowAnonLogging": true,
+      "contentVersion": "1.3.3.0.preview.2",
+      "panelizationSummary": {
+          "containsEpubBubbles": false,
+          "containsImageBubbles": false
+      },
+      "imageLinks": {
+          "smallThumbnail": "http://books.google.com/books/content?id=Gv7oh_ukn3QC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+          "thumbnail": "http://books.google.com/books/content?id=Gv7oh_ukn3QC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+      },
+      "language": "en",
+      "previewLink": "http://books.google.com/books?id=Gv7oh_ukn3QC&printsec=frontcover&dq=travel&hl=&cd=1&source=gbs_api",
+      "infoLink": "https://play.google.com/store/books/details?id=Gv7oh_ukn3QC&source=gbs_api",
+      "canonicalVolumeLink": "https://market.android.com/details?id=book-Gv7oh_ukn3QC",
+      "id": "Gv7oh_ukn3QC"
+  },
+  {
+      "title": "The Travel Book",
+      "subtitle": "A Journey Through Every Country in the World",
+      "authors": [
+          "Lonely Planet Publications (Firm)"
+      ],
+      "publishedDate": "2011",
+      "description": "TRAVEL & HOLIDAY GUIDES. Completely updated with 850 stunning new images; A -to-Z coverage of every country; Sumptuous double-page spreads with vivid photography and evocative text, Now in paperback format.",
+      "industryIdentifiers": [
+          {
+              "type": "ISBN_10",
+              "identifier": "1742200796"
+          },
+          {
+              "type": "ISBN_13",
+              "identifier": "9781742200798"
+          }
+      ],
+      "readingModes": {
+          "text": false,
+          "image": false
+      },
+      "pageCount": 447,
+      "printType": "BOOK",
+      "categories": [
+          "Travel"
+      ],
+      "averageRating": 4.5,
+      "ratingsCount": 3,
+      "maturityRating": "NOT_MATURE",
+      "allowAnonLogging": false,
+      "contentVersion": "preview-1.0.0",
+      "imageLinks": {
+          "smallThumbnail": "http://books.google.com/books/content?id=QfdOYgEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+          "thumbnail": "http://books.google.com/books/content?id=QfdOYgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+      },
+      "language": "en",
+      "previewLink": "http://books.google.com/books?id=QfdOYgEACAAJ&dq=travel&hl=&cd=2&source=gbs_api",
+      "infoLink": "http://books.google.com/books?id=QfdOYgEACAAJ&dq=travel&hl=&source=gbs_api",
+      "canonicalVolumeLink": "https://books.google.com/books/about/The_Travel_Book.html?hl=&id=QfdOYgEACAAJ",
+      "id": "QfdOYgEACAAJ"
+  },
+  {
+      "title": "Travel",
+      "publishedDate": "1914",
+      "industryIdentifiers": [
+          {
+              "type": "OTHER",
+              "identifier": "STANFORD:36105117062443"
+          }
+      ],
+      "readingModes": {
+          "text": false,
+          "image": true
+      },
+      "printType": "BOOK",
+      "categories": [
+          "Travel"
+      ],
+      "maturityRating": "NOT_MATURE",
+      "allowAnonLogging": false,
+      "contentVersion": "0.1.0.0.full.1",
+      "panelizationSummary": {
+          "containsEpubBubbles": false,
+          "containsImageBubbles": false
+      },
+      "imageLinks": {
+          "smallThumbnail": "http://books.google.com/books/content?id=nzJQAQAAIAAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+          "thumbnail": "http://books.google.com/books/content?id=nzJQAQAAIAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+      },
+      "language": "en",
+      "previewLink": "http://books.google.com/books?id=nzJQAQAAIAAJ&printsec=frontcover&dq=travel&hl=&cd=3&source=gbs_api",
+      "infoLink": "https://play.google.com/store/books/details?id=nzJQAQAAIAAJ&source=gbs_api",
+      "canonicalVolumeLink": "https://market.android.com/details?id=book-nzJQAQAAIAAJ",
+      "id": "nzJQAQAAIAAJ"
+  }]
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    
   }
 
   render() {
@@ -44,49 +169,7 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
-                        </div>
-                      </li>
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">Ender's Game</div>
-                          <div className="book-authors">Orson Scott Card</div>
-                        </div>
-                      </li>
-                    </ol>
-                  </div>
-                </div>
+                <BookSegment books={this.state.books} name="Currently Reading"/>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
